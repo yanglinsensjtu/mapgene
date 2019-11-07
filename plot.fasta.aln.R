e69ba1620480e6d79792188a42fcc571f3671016
lsyang <yanglinsensjtu@126.com>
2019-11-07 14:41:06 +0800

    msa plot function for fasta files

---
+++ plot.fasta.aln.R
@@ -0,0 +1,16 @@
+library(Biostrings)
+
+plotFastaAln2Pdf <- function(fasta.path = filepath, 
+                             title = title, 
+                             type = type){
+  path <- file.path(fasta.path)
+  fa <- switch (type,
+                DNA = readDNAStringSet(path),
+                protein = readAAStringSet(path),
+                RNA = readRNAStringSet(path))
+  aln <- msa(fa)
+  msaprintPDF(aln, title)
+}
+plotFastaAln2Pdf(fasta.path = '../cpf1proteins.fasta', 
+                 title = 'cpf1proteins.pdf', type = 'protein')
+
